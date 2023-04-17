0f9211cd65c689efc5aeeaacbdb7ffb8258d6cb2
Trym Hamer Gudvangen <trym.h.gudvangen@ntnu.no>
2023-04-17 16:39:20 +0200

    Init commit

---
+++ backend/src/test/java/edu/ntnu/idatt2106_2023_06/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idatt2106_2023_06.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
