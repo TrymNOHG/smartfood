a5f61b92f90fc3f910e8e62cec305edad6767470
BrageNTNU <bragehk@stud.ntnu.no>
2023-04-18 11:11:47 +0200

    feat: added file structure to backend with some empty classes

---
+++ backend/src/main/java/edu/ntnu/idatt2106_2023_06/backend/model/users/User.java
@@ -0,0 +1,4 @@
+package edu.ntnu.idatt2106_2023_06.backend.model.users;
+
+public class User {
+}
---
+++ backend/src/main/java/edu/ntnu/idatt2106_2023_06/backend/repo/users/UserRepository.java
@@ -0,0 +1,4 @@
+package edu.ntnu.idatt2106_2023_06.backend.repo.users;
+
+public interface UserRepository {
+}
