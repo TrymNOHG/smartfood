ddbfa6b380a06c079370be02a26e09d14334c8be
Tomas <tomas.beranek02@gmail.com>
2023-04-19 13:21:14 +0200

    feat: added basic button component

--- frontend/src/components/FridgeComponent.vue
+++ frontend/src/components/FridgeComponent.vue
@@ -1,9 +1,15 @@
 <template>
-  <h1>Fridge</h1>
+  <h1>{{ $t('fridges') }}</h1>
+  <list :items="['Item 1', 'Item 2', 'Item 3']"/>
+  <basic-button/>
 </template>
 
 <script>
+import List from "@/components/basic-components/listingComponent.vue";
+import BasicButton from "@/components/basic-components/BasicButton.vue";
+
 export default {
+  components: {BasicButton, List}
 
 };
 </script>
---
+++ frontend/src/components/basic-components/BasicButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <button>
+    {{ buttonText }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "BasicButton",
+
+  props: {
+    buttonText: {
+      type: String,
+      default: "Button"
+    },
+    buttonClass: {
+      type: String,
+      default: "basic-button"
+    }
+  }
+};
+</script>
+
+<style scoped>
+button {
+  background-color: #0F1CF3;
+  color: white;
+  width: 100px;
+  height: 33px;
+  border-radius: 5px;
+  font-size: 12px;
+  border: none;
+  outline: none;
+  transition: all 0.2s ease;
+}
+
+button:hover {
+  cursor: pointer;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+  background-color: #1e2eff;
+}
+</style>
