59d1ee82d3f89dd16c85f33043da920d7972cbe9
Trym Hamer Gudvangen <trym.gudvangen@gmail.com>
2023-04-19 16:38:11 +0200

    feat: initialize a fridge for each new user

--- backend/src/main/java/edu/ntnu/idatt2106_2023_06/backend/service/fridge/FridgeService.java
+++ backend/src/main/java/edu/ntnu/idatt2106_2023_06/backend/service/fridge/FridgeService.java
@@ -1,7 +1,11 @@
 package edu.ntnu.idatt2106_2023_06.backend.service.fridge;
 
-import edu.ntnu.idatt2106_2023_06.backend.model.FridgeItems;
+import edu.ntnu.idatt2106_2023_06.backend.exception.not_found.UserNotFoundException;
+import edu.ntnu.idatt2106_2023_06.backend.mapper.FridgeMemberMapper;
+import edu.ntnu.idatt2106_2023_06.backend.model.*;
+import edu.ntnu.idatt2106_2023_06.backend.repo.FridgeMemberRepository;
 import edu.ntnu.idatt2106_2023_06.backend.repo.FridgeRepository;
+import edu.ntnu.idatt2106_2023_06.backend.repo.users.UserRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -11,9 +15,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class FridgeService {
+public class FridgeService implements IFridgeService{
 
     private final FridgeRepository fridgeRepository;
+    private final FridgeMemberRepository fridgeMemberRepository;
+    private final UserRepository userRepository;
+
     private final Logger logger = LoggerFactory.getLogger(FridgeService.class);
 
 
@@ -21,6 +28,17 @@ public class FridgeService {
     public void init() {
         fridgeRepository.dropTrigger();
         fridgeRepository.createTrigger();
+    }
+
+    @Override
+    public void initializeFridge(String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException(username));
+
+        Fridge savedFridge = fridgeRepository.save(new Fridge("Home Fridge"));
+        //TODO: add ability to change fridge name
+
+        fridgeMemberRepository.save(FridgeMemberMapper.toFridgeMember(user, savedFridge, true));
     }
 
 }
