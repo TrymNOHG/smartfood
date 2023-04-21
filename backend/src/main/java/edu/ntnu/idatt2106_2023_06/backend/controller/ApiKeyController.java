2033a16f468544055c5ba49816cb5aa71fbecb49
BrageNTNU <bragehk@stud.ntnu.no>
2023-04-21 09:24:09 +0200

    feat: improved api key endpoint for kassal.

--- backend/src/main/java/edu/ntnu/idatt2106_2023_06/backend/controller/ApiKeyController.java
+++ backend/src/main/java/edu/ntnu/idatt2106_2023_06/backend/controller/ApiKeyController.java
@@ -1,7 +1,7 @@
 package edu.ntnu.idatt2106_2023_06.backend.controller;
 
 
-import edu.ntnu.idatt2106_2023_06.backend.config.ApiKeyConfig;
+import edu.ntnu.idatt2106_2023_06.backend.service.security.JwtService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,9 +18,13 @@ public class ApiKeyController {
     @Value("${kassal.api-key}")
     private String apiKey;
 
+    private final JwtService jwtService;
+
     @GetMapping(value="/get")
     @Operation(summary = "Add user to fridge")
     public ResponseEntity<Object> getApiKey() {
-        return ResponseEntity.ok(apiKey);
+        if(jwtService.isAuthenticated())
+            return ResponseEntity.ok(apiKey);
+        return ResponseEntity.badRequest().body("Not authenticated");
     }
 }
