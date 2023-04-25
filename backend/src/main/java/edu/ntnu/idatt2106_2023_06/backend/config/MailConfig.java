63e604e65bc48767a9037d6818c72cd4a1a1ec0c
Trym Hamer Gudvangen <trym.gudvangen@gmail.com>
2023-04-25 14:57:25 +0200

    fix: comment out profile in MailConfig

--- backend/src/main/java/edu/ntnu/idatt2106_2023_06/backend/config/MailConfig.java
+++ backend/src/main/java/edu/ntnu/idatt2106_2023_06/backend/config/MailConfig.java
@@ -10,7 +10,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
 
 @Configuration
-@Profile("!test")
+//@Profile("!test")
 public class MailConfig {
     @Value("${spring.mail.host}")
     private String host;
