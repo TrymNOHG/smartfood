f78f4b6d1c5c8d698d2de53c9a6146374e3db4b5
eirikelv <eirik.org@gmail.com>
2023-04-26 16:11:01 +0200

    feat: add alt text to enter fridge and exit fridge buttons

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
   <header v-if="!isOnRootPage">
 
     <router-link to="/fridges">
-      <img src="@/assets/images/smartmat.png" alt="Logo">
+      <img src="@/assets/images/smartmat_logo.png" alt="Logo">
     </router-link>
     <h1 class="matprat-title" :class="{ 'centered': isOnAuthPage }">{{ $t('matsmart') }}</h1>
     <nav :class="{ 'center-profile': !hasCurrentFridge }">
@@ -60,8 +60,8 @@
       </h1>
       
       <router-link class="change-button" to="/fridges" >
-        <img src="@/assets/images/exit_change_fridge.png" style="max-height: 100%" v-if="hasCurrentFridge">
-        <img src="@/assets/images/enter_choose_fridge.png" style="max-height: 100%" v-else>
+        <img src="@/assets/images/exit_change_fridge.png" :alt=" $t('exit-fridge') " style="max-height: 100%" v-if="hasCurrentFridge">
+        <img src="@/assets/images/enter_choose_fridge.png" :alt=" $t('enter-fridge') " style="max-height: 100%" v-else>
 
       </router-link>
     </div>
