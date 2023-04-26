df855f4055bf71cc437014ad1007e888bce3e859
DeDcon0 <bekadb@ntnu.no>
2023-04-26 15:15:02 +0200

    feat/cypress e2e testing progress on first two tests (by mocking backend)

--- frontend/cypress/e2e/Shopping_Cart.cy.js
+++ frontend/cypress/e2e/Shopping_Cart.cy.js
@@ -1,35 +1,102 @@
+describe('Start to end: ', () => {
+  const base_url_site = "http://localhost:5173";
+  const base_url_endpoint = "http://localhost:8080";
 
-describe('Cart', () => {
+  beforeEach(() => {
+    cy.intercept('POST', 'http://localhost:8080/user/login', {
+      statusCode: 200,
+      body: {
+        token: 'my-user-token'
+      }
+    }).as('loginRequest')
 
-  it('should login an already existing  user', () =>{
-    cy.request('POST', 'http://localhost:8080/user/login', {
-      email: 't@t.t',
-      password: '12345678'
-    }).then((response) => {
-    });
+    cy.intercept('GET', `${base_url_endpoint}/user/get/info`, (req) => {
+      //req.headers['Authorization'] = `Bearer my-user-token`
+      req.reply({
+        statusCode: 200,
+        body: {
+          userId: 123,
+          username: 'johndoe',
+          firstName: 'John',
+          lastName: 'Doe', email: 'johndoe@example.com'
+        }
+      })
+    }).as('fetchUser')
+
+    cy.intercept('GET', `${base_url_endpoint}/fridge/loadAll?user=johndoe`, (req) => {
+        req.reply({
+          statusCode: 200,
+          body: {
+            fridgeDTOS: [
+              {
+                fridgeId: 88,
+                fridgeName: 'Home Fridge'
+              }
+            ]
+          }
+        })
+    }).as('loadAllFridges')
+
+
+
+
+    cy.visit(`${base_url_site}`)
+
+    cy.contains('a', 'Logg inn').click()
+
+    cy.get('input[name="email"]').type('t@t.t')
+    cy.get('input[name="password"]').type('12345678')
+
+    cy.get('button[type="submit"]').click()
+
+    cy.wait('@loginRequest')
+    cy.wait('@fetchUser')
+    cy.wait('@loadAllFridges')
   })
 
-  //CHOOSING A FRIDGE FOR LATER TESTS
+  it('should redirect authenticated users to fridges view on login', () => {
+    cy.url().should('include', '/fridges')
+  })
+
   it('should display the fridges after logging in', () => {
-    cy.visit('http://localhost:5173/fridges', {
-      onBeforeLoad(win) {
-        const sessionToken = window.localStorage.getItem('sessionToken');
-        win.sessionStorage.setItem('sessionToken', sessionToken);
+    cy.visit(`${base_url_site}/fridges`);
+    cy.contains('Home Fridge').should('be.visible');
+  });
+});
+
+describe('After choosing fridge', () => {
+  const base_url = "http://localhost:5173";
+
+  beforeEach(() => {
+    cy.intercept('POST', 'http://localhost:8080/item/shopping/add', {
+      statusCode: 200,
+      body: {
+        success: true,
+        message: 'Item added to cart'
       }
-    });
+    }).as('addItemRequest')
 
-    cy.get('.item').should('contain', 'Home Fridge');
-  });
+    cy.visit(`${base_url}`)
 
+    cy.contains('a', 'Logg inn').click()
 
+    cy.get('input[name="email"]').type('t@t.t')
+    cy.get('input[name="password"]').type('12345678')
 
-  //MUST CHOOSE A FRIDGE BEFORE ACCESSING CART PAGE
+    cy.get('button[type="submit"]').click()
+
+    cy.contains('Home Fridge').click()
+  })
+
+  it('should add test item to cart', () => {
+    cy.get('#searchbar').type('Milk{enter}')
+    cy.wait('@addItemRequest')
+  })
+
   it('should display the cart items after logging in', () => {
-    cy.visit('http://localhost:5173/cart');
-
+    cy.visit(`${base_url}/cart`);
     cy.get('#cart-item-1').should('contain', 'Milk');
-    cy.get('#cart-item-2').should('contain', 'Bread');
-  });
+  })
 
   it('should trigger handleSearch function when search button is clicked', () => {
     cy.visit('http://localhost:5173/cart', {
@@ -41,7 +108,7 @@ describe('Cart', () => {
 
     cy.get('#searchbtn').click();
     cy.get('@handleSearch').should('have.been.calledOnce');
-  });
+  })
 
   it('should add an item to the cart when addItemToList is called', () => {
     cy.visit('http://localhost:5173/cart', {
@@ -55,5 +122,5 @@ describe('Cart', () => {
     cy.get('@addItemToList').should('have.been.calledOnce');
 
     cy.get('#cart-item-3').should('contain', 'Eggs');
-  });
-});
+  })
+})
--- frontend/src/services/FridgeServices.js
+++ frontend/src/services/FridgeServices.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import SessionToken from '@/features/SessionToken.js'
 
 const BASE_LISTING_URL = "http://localhost:8080/fridge";
-
 export const getAllFridges = async (username) => {
     return await axios.get(`${BASE_LISTING_URL}/loadAll?user=${username}`);
 }
