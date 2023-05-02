ac86ddabd36eb46c45f66c1511df89641d1570d6
eirikelv <eirik.org@gmail.com>
2023-05-02 11:23:17 +0200

    fix: change id:"welcome" to have no text

--- frontend/src/views/FridgesView.vue
+++ frontend/src/views/FridgesView.vue
@@ -341,7 +341,7 @@ export default {
           },
             {
                 id: 'Welcome',
-                text:  `${$t("welcome")}` ,
+                text:  `` ,
                 buttons:[
                     {
                         action: function () {
