204f900b1dd8fb807de4a582b7cfca522a5eed34
Trym Hamer Gudvangen <trym.gudvangen@gmail.com>
2023-05-02 09:54:09 +0200

    feat: add endpoint to auto-add ingredients to shopping cart

--- backend/src/main/java/edu/ntnu/idatt2106_2023_06/backend/controller/RecipeController.java
+++ backend/src/main/java/edu/ntnu/idatt2106_2023_06/backend/controller/RecipeController.java
@@ -2,8 +2,12 @@ package edu.ntnu.idatt2106_2023_06.backend.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.ntnu.idatt2106_2023_06.backend.dto.items.shopping_list.RecipeShoppingDTO;
 import edu.ntnu.idatt2106_2023_06.backend.dto.recipe.RecipeLoadDTO;
+import edu.ntnu.idatt2106_2023_06.backend.exception.UnauthorizedException;
 import edu.ntnu.idatt2106_2023_06.backend.model.recipe.Recipe;
+import edu.ntnu.idatt2106_2023_06.backend.service.items.ItemRecipeScoreService;
+import edu.ntnu.idatt2106_2023_06.backend.service.items.ItemService;
 import edu.ntnu.idatt2106_2023_06.backend.service.items.RecipeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -19,6 +23,7 @@ import org.slf4j.LoggerFactory;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -30,6 +35,8 @@ import java.io.IOException;
 public class RecipeController {
 
     private final RecipeService recipeService;
+    private final ItemRecipeScoreService itemRecipeScoreService;
+    private final ItemService itemService;
     private final Logger logger = LoggerFactory.getLogger(RecipeController.class);
 
     @GetMapping(value="/download")
@@ -114,6 +121,38 @@ public class RecipeController {
         return ResponseEntity.ok(recipeLoadDTOs);
     }
 
+    //TODO: use test to see how effective the algorithm is. Does it need fine tuning.
+    @PostMapping(value="/generateScoresTest")
+    @Operation(summary = "Generate item recipe scores")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "This endpoint generates item recipes scores between all items and recipes",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Object.class)) })}
+    )
+    public ResponseEntity<Object> generateItemRecipeScoresTest(@ParameterObject @RequestParam(name="item") Long itemId,
+                                                           @ParameterObject @RequestParam(name="recipe") Long recipeId) {
+
+        itemRecipeScoreService.testScore(itemId, recipeId);
+        return ResponseEntity.ok().build();
+    }
+
     //TODO: add more recipe load things, including pagination of random recipes.
+
+    @PostMapping(value="/addIngredients")
+    @Operation(summary = "Add ingredients to shopping list")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "This endpoint adds ingredients to shopping list",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Object.class)) })}
+    )
+    public ResponseEntity<Object> addIngredientToShoppingList(@ParameterObject @RequestBody RecipeShoppingDTO recipeShoppingDTO,
+                                                              Authentication authentication) {
+        if(authentication == null || !authentication.isAuthenticated()) throw new UnauthorizedException("Anon");
+
+        logger.info("Attempting to add ingredients to shopping list.");
+        itemService.addIngredientsToShoppingList(recipeShoppingDTO, authentication.getName());
+        logger.info("Ingredients have been added.");
+        return ResponseEntity.ok().build();
+    }
 
 }
