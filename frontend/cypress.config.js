0801849ec445b84722d263be1b870887de62cf31
DeDcon0 <bekadb@ntnu.no>
2023-05-02 18:39:17 +0200

    fix/ testing env

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,7 +1,13 @@
-module.exports = {
-  env: {
-    codeCoverage: {
-      url: "../coverage",
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      on("before:browser:launch", (browser, launchOptions) => {
+        // set the baseUrl configuration option
+        launchOptions.args.push("--baseUrl=http://localhost:5173");
+        return launchOptions;
+      });
     },
   },
-};
+});
