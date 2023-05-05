0fa60aba3d8ae5a7d79565cebc4e7b3bdb392cd0
Eilert <eilertwh@stud.ntnu.no>
2023-05-05 21:43:00 +0200

    added mock token

--- frontend/src/features/SessionToken.js
+++ frontend/src/features/SessionToken.js
@@ -1,13 +1,23 @@
-import {useLoggedInStore} from "@/store/store";
+import { useLoggedInStore } from "@/store/store";
 import router from "@/router/router";
 
-export default async function sessionToken () {
-    const sessionToken = useLoggedInStore().getSessionToken
-    if (sessionToken === null) {
-        alert("Log in to access your profile!") //TODO: make better
-        await router.push("/login")
-        throw new Error("Session token cannot be null. Login in again.")
+let mockToken = null;
+
+export function setMockToken(token) {
+    mockToken = token;
+}
+
+export default async function sessionToken() {
+    if (mockToken) {
+        return mockToken;
     }
 
-    return sessionToken
+    const sessionToken = useLoggedInStore().getSessionToken;
+    if (sessionToken === null) {
+        alert("Log in to access your profile!"); //TODO: make better
+        await router.push("/login");
+        throw new Error("Session token cannot be null. Login in again.");
+    }
+
+    return sessionToken;
 }
